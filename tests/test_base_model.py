3d8d3237a38503c42bb999d182e2c543b52813d4
yasoug <ougouisyassine@gmail.com>
2023-08-13 03:29:20 -0700

    initiate tests

---
+++ tests/test_base_model.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Unittest for BaseModel class
+"""
+import json
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+import models
+import sys
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    classss for base model test
+    """
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
